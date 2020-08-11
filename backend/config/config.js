e8630ff17a0873088794d590672546226b5e78d9
pius712 <aksfbsgnlfls@gmail.com>
2020-08-11 20:53:28 +0900

    fix: config

--- backend/config/config.js
+++ backend/config/config.js
@@ -19,7 +19,7 @@ module.exports = {
 	production: {
 		username: 'root',
 		password: null,
-		database: 'database_production',
+		database: process.env.DB_PASSWORD,
 		host: '127.0.0.1',
 		dialect: 'mysql',
 	},
